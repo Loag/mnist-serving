9496598991b8ca8597ddaba9dae003b4ceae1238
Logan <10239411+Loag@users.noreply.github.com>
2023-12-17 01:28:39 +0000

    building placeholder backend

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mnist-serving
+
+## setup
+
+### c++ grpc
+
+``` bash
+    sudo apt-get install libgrpc++-dev libprotobuf-dev protobuf-compiler-grpc
+```
+
+``` bash
+# c++
+protoc -I . --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` *.proto
+protoc --cpp_out=. *.proto
+```
+
+### golang grpc
+
+``` bash
+
+# install protobuf go compiler 
+ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+ go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+ # add to path
+ $ export PATH="$PATH:$(go env GOPATH)/bin"
+```
+
+``` bash
+# build golang bindings
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative *.proto
+```
---
+++ backend/src/main.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "onnxruntime/onnxruntime_cxx_api.h"
+#include "service.pb.h"
+#include "service.grpc.pb.h"
+#include <grpcpp/grpcpp.h>
+
+using namespace std;
+using namespace routing;
+
+class InferenceRPC final : public InferenceService::Service {
+    grpc::Status Infer(grpc::ServerContext* context, const InferenceRequest* request, InferenceResponse* response) override {
+        response->set_number("1");
+        return grpc::Status::OK;
+    }
+};
+
+void RunServer() {
+    auto server_address("0.0.0.0:50051");
+
+    InferenceRPC service;
+    grpc::ServerBuilder builder;
+
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+
+    unique_ptr<grpc::Server> server(builder.BuildAndStart());
+
+
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown.
+    server->Wait();
+}
+
+
+int main(int argc, char* argv[]) {
+    try {
+        RunServer();
+
+    } catch (const exception& err) {
+        cerr << "Error: " << err.what() << endl;
+        return 1;
+    }
+
+    return 0;
+}
